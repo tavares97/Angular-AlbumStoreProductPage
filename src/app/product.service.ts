f1731889e2f33b85a1433785d0cddcfe40362cd8
Andre Filipe Peneiras Tavares <andre.tavares@siemens.com>
2020-01-02 10:27:29 +0000

    List of products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import { Album } from './album';
+import { Product } from './product';
 
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
@@ -10,6 +11,7 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {
 
@@ -18,5 +20,11 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(
       (response) => <Album>response.json());
+  }
+
+  getproducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(
+      (response) => <Product[]>response.json()
+    );
   }
 }
---
+++ src/app/product.ts
@@ -0,0 +1,5 @@
+export interface Product {
+    id: number;
+    artistNumber: string;
+    albumName: string;
+}
